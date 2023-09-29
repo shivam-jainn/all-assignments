82ea9b2a49fa175e8f88d8d6da054437ac0c6bdf
shivam-jainn <shivam.clgstash@gmail.com>
2023-09-29 11:11:40 +0530

    file server assignment completed

--- week-2/Week-2-Assignments/02-nodejs/fileServer.js
+++ week-2/Week-2-Assignments/02-nodejs/fileServer.js
@@ -22,4 +22,35 @@ const path = require('path');
 const app = express();
 
 
+app.get('/files', (req,res)=>{
+ const filePath = path.join(__dirname, './files/');
+  fs.readdir(filePath,(err,data)=>{
+    if(err){
+      res.sendStatus(500);
+    }
+
+    res.status(200).json(data);
+  })
+})
+
+
+app.get('/file/:filename', (req,res)=>{
+  const filePath = path.join(__dirname, './files/', req.params.filename);
+
+  fs.readFile(filePath,'utf-8',(err,data)=>{
+    if(err){
+    res.status(404).send("File not found")
+    };
+
+    res.status(200).send(data);
+  });
+
+})
+
+
+app.all('*',(req,res)=>{
+  res.status(404).send('Route not found');
+})
+
+// app.listen(3000);
 module.exports = app;
